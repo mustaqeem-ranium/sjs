dd03ac2167f81dde9fec9a37bc72a1e61c62c5d5
mustaqeem-ranium <59199635+mustaqeem-ranium@users.noreply.github.com>
2020-10-01 19:12:52 +0530

    Add files via upload
    product page added.

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,11 @@
 $('.toggleDropdown').on('click', function() {
     $('.profile-dropdown').toggleClass('hidden');
 });
+
+$('.open-menu').on('click', function () {
+    $('.mobile-menu').removeClass('hidden');
+});
+
+$('.close-menu').on('click', function () {
+    $('.mobile-menu').addClass('hidden');
+});
